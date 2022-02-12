8836da3e6d2e39cc5836655b7d9b46c5bba34673
rinoshan <rinoshan@hotmail.fr>
2022-02-12 01:07:11 +0100

    first commit

---
+++ src/DataPersister/UserDataPersister.php
@@ -0,0 +1,49 @@
+<?php 
+
+
+namespace App\DataPersister;
+
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+final class UserDataPersister implements ContextAwareDataPersisterInterface
+{
+
+    private EntityManagerInterface $entityManager;
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->entityManager = $entityManager;
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
+
+
+    public function supports($data, array $context = []): bool
+    {
+        return $data instanceof User;
+    }
+
+    public function persist($data, array $context = [])
+    {
+        $this->hashPassword($data);
+        $this->entityManager->persist($data);
+        $this->entityManager->flush();
+    }
+
+    public function remove($data, array $context = [])
+    {
+        $this->entityManager->remove($data);
+        $this->entityManager->flush();
+    }
+
+    private function hashPassword(User $user) 
+    {
+        $hashedPassword = $this->userPasswordHasher->hashPassword($user,$user->getPassword());
+        $user->setPassword($hashedPassword);
+    }
+}
